be020c15b3360a50f1654cb422e866f5babbe08c
Joao P G Begiato <jpbegiato@hotmail.com>
2022-06-30 01:51:16 -0300

    fix query pergunta 2

--- query/query_pergunta_2.sql
+++ query/query_pergunta_2.sql
@@ -5,4 +5,4 @@ FROM
         INNER JOIN
     steam_main ON steam_ux.app_id = steam_main.app_id
         
-order by CAST(steam_ux.positive_ratings as decimal) desc
+order by CAST(steam_ux.negative_ratings as decimal) desc
