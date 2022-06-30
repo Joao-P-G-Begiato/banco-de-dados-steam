79e2c680d6341dbf1b35addf6920d89cb13c04f9
systemilc <isaac.systemilc@gmail.com>
2022-06-30 00:21:44 -0300

    adicionado pergunta 3

---
+++ query/query_pergunta_3.sql
@@ -0,0 +1,8 @@
+SELECT 
+    steam_main.genres, steam_ux.average_playtime
+FROM
+    steam_main
+        INNER JOIN
+    steam_ux ON steam_ux.app_id = steam_main.app_id
+GROUP BY steam_main.genres
+ORDER BY CAST(steam_main.genres AS DECIMAL) DESC
