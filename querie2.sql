7fa7ba0a9e704a835107fb947551af220ed14249
Roberta <roberttavalentim07@icloud.com>
2022-06-29 23:17:10 -0300

    querie 1 e 2

---
+++ querie2.sql
@@ -0,0 +1,8 @@
+SELECT 
+    steam_main.app_name, steam_ux.negative_ratings
+FROM
+    steam_ux
+        INNER JOIN
+    steam_main ON steam_ux.app_id = steam_main.app_id
+        
+order by CAST(steam_ux.positive_ratings as decimal) desc
